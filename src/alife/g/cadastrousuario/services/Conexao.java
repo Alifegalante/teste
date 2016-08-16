7953aa6de77fec18b87b7028b4c2f68c1fa6f85c
Alife Galante <Alifegalante@hotmail.com>
2016-08-15 21:17:15 -0300

    Commit Inical Pelo Netbeans Cadastro de usuário inserindo

---
+++ src/alife/g/cadastrousuario/services/Conexao.java
@@ -0,0 +1,22 @@
+package alife.g.cadastrousuario.services;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexao {
+   
+    private static Connection conn;
+
+    private Conexao() throws SQLException {
+        if (Conexao.conn==null) {
+         Conexao.conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/cad_usuario?zeroDateTimeBehavior=convertToNull","root","");
+        }
+    }
+    
+    public static Connection getInstance() throws SQLException{
+        new Conexao();
+        return Conexao.conn;
+    }
+    
+}
---
+++ src/alife/g/cadastrousuario/to/UsuarioController.java
@@ -0,0 +1,59 @@
+package alife.g.cadastrousuario.to;
+
+import alife.g.cadastrousuario.dao.domain.Usuario;
+import alife.g.cadastrousuario.model.dao.UsuarioDao;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JOptionPane;
+
+public class UsuarioController {
+ 
+    private  UsuarioDao dao;
+    private Usuario usuarioManipulado;
+    private List<Usuario> lista;
+
+    public UsuarioController() {
+        this.dao = new UsuarioDao();
+        this.usuarioManipulado = new Usuario();
+        this.lista = new ArrayList<Usuario>();
+    }
+    
+    public void pesquisar(){ 
+      this.lista = dao.listar();
+    }
+    
+    public void novo(){ 
+      this.usuarioManipulado = new Usuario();             
+    }
+    
+     public void editar(){ 
+            
+    }
+    
+    public void excluir(){ 
+            
+      this.dao.excluir(this.usuarioManipulado);
+      }
+    
+    public void salvar(){ 
+      this.dao.salvar(this.usuarioManipulado);
+    }
+
+    public Usuario getUsuarioManipulado() {
+        return usuarioManipulado;
+    }
+
+    public void setUsuarioManipulado(Usuario usuarioManipulado) {
+        this.usuarioManipulado = usuarioManipulado;
+    }
+
+    public List<Usuario> getLista() {
+        return lista;
+    }
+
+    public void setLista(List<Usuario> lista) {
+        this.lista = lista;
+    }
+    
+}
+
